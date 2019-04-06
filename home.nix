690f4ae20879f4c7263a1bba96d262162194c509
Katharina Fey <kookie@spacekookie.de>
2019-04-06 12:09:33 +0200

    Repository housekeeping

--- home.nix
+++ home.nix
@@ -36,7 +36,7 @@ let
         sha256 = "0akyspxl80h2kgp6nhbhnz9v5265pi6d57i6l90pf50l92z61sw7";
       };
       nativeBuildInputs = nativeBuildInputs ++ [ pkgs.autoreconfHook ];
-      patches = patches ++ [ ./0001-Make-spacekookie-fit-untruncated-as-a-user-name.patch ];
+      patches = patches ++ [ ./0001-htop-untruncated-username.patch ];
     }));
   git = (pkgs.git.override { svnSupport = true; sendEmailSupport = true;  });
   iosevka = (pkgs.iosevka.override {
