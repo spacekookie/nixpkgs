bddaaac86c00c757c00d9ae7804f2a0e5d1ca24a
Katharina Fey <kookie@spacekookie.de>
2019-08-03 14:54:41 +0200

    kakoune: switching to nixos-unstable version and tweaking config
    The tab handling in this commit is not ideal. It has some good
    ideas in it, but definitely needs more work. Probabla a TODO
    but maybe figure out how using tabs should work, then try to
    implement something along those lines

--- kakoune/default.nix
+++ kakoune/default.nix
@@ -1,22 +1,11 @@
 { pkgs, ... }:
 
-let
-  package = (pkgs.kakoune.overrideAttrs ({ src, makeFlags, ... }: {
-      src = pkgs.fetchFromGitHub {
-        repo = "kakoune";
-        owner = "mawww";
-        rev = "89cd68d8aff07792b03a0affc19dbb01f036f554";
-        sha256 = "0nc71jl2bpzzx3daqhfjgmmf3fh9k3gj1y1j536xnybd78vvgxq1";
-       };
-      enableParallelBuilding = true;
-    }));
-in
-  {
-    home.packages = [ package ]; 
+{
+  home.packages = [ pkgs.kakoune ];
 
-    xdg.configFile."kak/" = {
-      recursive = true;
-      executable = false;
-      source = ./.;
-    };
-  }
+  xdg.configFile."kak/" = {
+    recursive = true;
+    executable = false;
+    source = ./.;
+  };
+}
