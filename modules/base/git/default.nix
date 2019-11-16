4bb5ae2e813f0d881f6a45608bcc01dd5af44ffd
Katharina Fey <kookie@spacekookie.de>
2019-11-16 16:45:49 +0100

    base/git: Adding mercurial support

--- modules/base/git/default.nix
+++ modules/base/git/default.nix
@@ -4,6 +4,7 @@ let
   git = (pkgs.git.override { svnSupport = true; sendEmailSupport = true; });
 in
 {
-  environment.systemPackages = [ git ]
-                               ++ (with pkgs; [ gitAndTools.hub ]);
+  environment.systemPackages = [ git ] ++
+                               (with pkgs.gitAndTools;
+                                 [ hub git-remote-hg ]);
 }
