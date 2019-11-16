7951db6155684fc73840603163d1e09bb87356de
Katharina Fey <kookie@spacekookie.de>
2019-11-16 16:52:41 +0100

    ws/networking: Enabling plausible MAC randomisation

--- modules/workstation/networking/default.nix
+++ modules/workstation/networking/default.nix
@@ -1,11 +1,22 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  environment.systemPackages = [ pkgs.mullvad-vpn ];
+
   networking = {
     networkmanager = {
       enable = true;
+
+      # Plausible MAC randomization
+      ethernet.macAddress = "random";
+      wifi.macAddress = "random";
       extraConfig = ''
+        # Integrate networkmanager with resolvd
         rc-manager="resolvconf";
+
+        [connection-extra]
+        ethernet.generate-mac-address-mask=FE:FF:FF:00:00:00
+        wifi.generate-mac-address-mask=FE:FF:FF:00:00:00
       '';
     };
 
